bf22bc8c5568180c3e9716859a0640f9010ee49e
mike22a <43526753+mike22a@users.noreply.github.com>
2025-09-13 18:07:27 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://5dc1af51-5cb6-41f4-bf9e-033a6784430d-00-a3di2yp1zx2h.riker.replit.dev/api' // Add your API URL here
+  apiUrl: 'https://5dc1af51-5cb6-41f4-bf9e-033a6784430d-00-1oietborliz8j.pike.replit.dev/api' // Add your API URL here
 };
